d01d6195475da9c7cb855882fca62600312e4557
Hannes <hannes.sjo2@gmail.com>
2024-09-09 19:41:40 +0300

    Fixing stuffs

--- RPI/main.py
+++ RPI/main.py
@@ -1,9 +1,12 @@
 from kivy.app import App
 from kivy.uix.button import Label
-
+from kivy.core.window import Window
 
 class HelloWorldApp(App):
+    size = 800; 
+
     def build(self):
+        Window.size = (self.size, self.size);
         return Label(text="Hello world")
 
 

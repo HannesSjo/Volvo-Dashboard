5d4afb27873eb50da31c8008a0268e880a9a75ae
Capppe <casper.nordstrom@ha.ax>
2024-09-09 19:21:17 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.button import Label
+
+
+class HelloWorldApp(App):
+    def build(self):
+        return Label(text="Hello world")
+
+
+if __name__ == '__main__':
+    HelloWorldApp().run()

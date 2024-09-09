f103b9fc2cd6aebe6e28843cbb4cf7d3fdd684e7
Hannes <hannes.sjo2@gmail.com>
2024-09-09 21:00:16 +0300

    Added DataFetcherDemo functionality

---
+++ RPI/dataFetcherDemo.py
@@ -0,0 +1,37 @@
+import threading
+import time
+import json
+
+class DataFetcher:
+    def __init__(self, data_lock, shared_data):
+        self.data_lock = data_lock
+        self.shared_data = shared_data
+        self.thread = threading.Thread(target=self.loop)
+        self.thread.daemon = True
+        self.is_running = True
+
+    def start(self):
+        with open('template.json', 'r') as template:
+            self.data = json.load(template)
+        with self.data_lock:
+            self.shared_data.clear()
+            self.shared_data.update(self.data)
+
+        self.thread.start()
+
+    def loop(self):
+        while self.is_running:
+            self.fetchData()
+
+            with self.data_lock:
+                self.shared_data.clear()
+                self.shared_data.update(self.data)
+
+            time.sleep(0.2)
+
+    def fetchData(self):
+        return ""
+
+    def stop(self):
+        self.is_running = False
+        self.thread.join()
--- RPI/main.py
+++ RPI/main.py
@@ -1,14 +1,42 @@
 from kivy.app import App
-from kivy.uix.button import Label
+from kivy.uix.label import Label
 from kivy.core.window import Window
+from kivy.clock import Clock
+#from dataFetcher import DataFetcher
+from dataFetcherDemo import DataFetcher
+import threading
 
-class HelloWorldApp(App):
-    size = 800; 
+class Dashboard(App):
+    size = 800
+    fps = 24
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.data_lock = threading.Lock()
+        self.shared_data = {}
+        self.label = None
+        self.data_fetcher = DataFetcher(self.data_lock, self.shared_data)
 
     def build(self):
-        Window.size = (self.size, self.size);
-        return Label(text="Hello world")
+        self.data_fetcher.start()
+        Window.size = (self.size, self.size)
 
+        self.label = Label()
+
+
+        Clock.schedule_interval(self.update_display, (1/self.fps))
+        return self.label
+
+    def on_stop(self):
+        self.data_fetcher.stop()
+
+    def update_display(self, dt):
+        if self.shared_data == {}:
+            return
+        
+        with self.data_lock:
+            AFR = str(self.shared_data['AFR'])
+            self.label.text = AFR
 
 if __name__ == '__main__':
-    HelloWorldApp().run()
+    Dashboard().run()
